de4db53de0016f3af88b9c5a7b90ee2d5fc9441b
Gabe <17620304+GitableGabe@users.noreply.github.com>
2023-04-22 19:25:06 -0400

    test5

--- module6/d3_lab/01answer.js
+++ module6/d3_lab/01answer.js
@@ -1,22 +1,20 @@
 d3.csv('ue_industry.csv', data => {
 
-    // Define your scales and generator here.
-    var x = d3.scaleLinear()
+    const xScale = d3.scaleLinear()
         .domain(d3.extent(data, d => +d.index))
-        .range([20, 580]);
+        .range([20, 1180]);
 
-    var y = d3.scaleLinear()
+    const yScale = d3.scaleLinear()
         .domain(d3.extent(data, d => +d.Agriculture))
-        .range([20, 580]);
+        .range([580, 20]);
 
-    let line = d3.line()
+    const line = d3.line()
         .x(d => xScale(+d.index))
         .y(d => yScale(+d.Agriculture));
 
     d3.select('#answer1')
-        // append more elements here
         .append('path')
         .attr('d', line(data))
-        .attr('stroke', '#2e2928')
+        .attr('stroke', '474a4f');
 
 });
