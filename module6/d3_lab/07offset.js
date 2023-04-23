3670fc3bf84ec08ce4405539f3bf9a9ae7c76be8
Gabe <17620304+GitableGabe@users.noreply.github.com>
2023-04-23 19:22:45 -0400

    offset wiggle

--- module6/d3_lab/07offset.js
+++ module6/d3_lab/07offset.js
@@ -19,7 +19,7 @@ d3.csv('ue_industry.csv', data => {
         .range([20, 1180]);
     
     const yScale = d3.scaleLinear()
-        .domain([0, totalYmax]).nice()
+        .domain([0, totalYmax])
         .range([580, 20]);
 
     const fillScale = d3.scaleOrdinal()
@@ -28,7 +28,7 @@ d3.csv('ue_industry.csv', data => {
 
     const stackLayout = d3.stack()
         .keys(industries)
-        .offset(d3.stackOffsetSilhouette);
+        .offset(d3.stackOffsetWiggle);
         
     const stackArea = d3.area()
         .x((d, i) => xScale(i))
